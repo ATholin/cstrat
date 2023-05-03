b7da65eab6a4f174730ed8a700003be0822a0af4
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2023-05-03 20:09:29 +0200

    Remove app directory

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import NextAuth from "next-auth";
-import { createAuthOptions } from "~/server/auth";
-
-function handler(req: NextApiRequest, res: NextApiResponse) {
-    return NextAuth(req, res, createAuthOptions(req))
-}
-
-export { handler as GET, handler as POST };
